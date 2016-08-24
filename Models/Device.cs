bffb473caa62ce8492f88681af8c9ba6663eb429
Julio Pozo <jpozo@acecashexpress.com>
2016-08-24 14:21:23 -0500

    Fetcher Service Model Refactoring

---
+++ Models/Device.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PlatanoApi.Model
+{
+    public class Device
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string State { get; set; }
+    }
+}
