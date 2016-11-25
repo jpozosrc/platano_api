0875b3b28929d68a5a441c1e3877e4dd4aaae38a
julio <jpozo@a.com>
2016-11-25 10:13:07 -0600

    Fix db connection string

--- Controllers/AutomationCommand.cs
+++ Controllers/AutomationCommand.cs
@@ -16,25 +16,34 @@ namespace PlatanoApi.WebApi.Controllers
         }
 
         [HttpGet]
-        public AutomationCommand[] GetNextCommand()
+        public IActionResult Get()
         {
-            return _context.AutomationCommand.ToArray();
+            try
+            {
+                return Ok(_context.AutomationCommand.ToArray());
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
         }
 
         [HttpPost]
-        public void Add([FromBody] AutomationCommand cmd)
+        public IActionResult Add([FromBody] AutomationCommand cmd)
         {
             _context.AutomationCommand.Add(cmd);
             _context.SaveChanges();
+            return Ok();
         }
 
         [HttpDelete]
         [Route("{id}")]
-        public void Delete([FromRoute] int id)
+        public IActionResult Delete([FromRoute] int id)
         {
             var cmd = _context.AutomationCommand.Where(i => i.Id == id).Single();
             _context.AutomationCommand.Remove(cmd);
             _context.SaveChanges();
+            return Ok();
         }
     }
 }
